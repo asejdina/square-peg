e10d9de6d2fbeb21b49c5611ee7b9f9224dd4f2e
Robert Paterno <bobpaterno@gmail.com>
2014-06-07 14:47:39 -0500

    ipAddress is now saved as a string

--- app/models/user.js
+++ app/models/user.js
@@ -76,8 +76,12 @@ class User {
       }
       this.name = fields.name[0];
       this.ipAddress = fields.ipAddress[0].toString();
-      this.ipAddress = this.ipAddress.match(/[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/g,''); // check if valid IP address
+      this.ipAddress = this.ipAddress.match(/[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/g,'').toString(); // check if valid IP address
       if(this.ipAddress === ''){ this.ipAddress = '000.000.000.000'; }
+console.log('[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]');
+console.log(fields.ipAddress[0]);
+console.log(this.ipAddress);
+console.log(typeof(this.ipAddress));
       this.bio = fields.bio[0];
       this.seeking = fields.seeking[0].toLowerCase().replace(/,/g,' ').split(' ').filter(Boolean);
       this.languages = fields.languages[0].toLowerCase().replace(/,/g,' ').split(' ').filter(Boolean);

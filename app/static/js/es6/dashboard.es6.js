e1233a83706a0b22ff3dd43907b700749c431741
asejdina <asejdina@gmail.com>
2014-06-07 16:26:38 -0500

    ajax calls need work

--- app/static/js/es6/dashboard.es6.js
+++ app/static/js/es6/dashboard.es6.js
@@ -1,11 +1,33 @@
+/* global ajax */
+/* jshint unused:false */
+
 (function(){
   'use strict';
 
   $(document).ready(init);
 
   function init(){
-    
+    getMatches();
+    getPings();
+    getMessageNumber();
   }
+
+  function getMatches(){
+    ajax(`/users/top3matches`, 'get', null, html=>{
+      $('#topMatches').append(html);
+    });
+  }
+
+  function getPings(){
+    ajax(`/pings/:toId`, 'get', null, html=>{
+      $('#ping').append(html);
+    });
+  }
+
+  function getMessageNumber(){
+
+  }
+
 
 
 })();

be27c2f19ccb3619ff485fcd8022fbef09961ee5
Aaron Johnson <ajohnson13.aj@gmail.com>
2014-06-08 12:34:23 -0500

    homepage styling

---
+++ app/static/js/es6/main.es6.js
@@ -0,0 +1,54 @@
+/* exported ajax */
+
+(function(){
+  'use strict';
+
+  $(document).ready(init);
+
+  function init(){
+    //alert('loaded');
+    $('#modal').hide();
+    $('#show-login').click(showModalLogin);
+    $('#show-register').click(showModalRegister);
+    $('#getstarted').click(showModalRegister);
+    $('#form-register').hide();
+    $('#btn-register').click(showRegister);
+    $('#btn-login').click(showLogin);
+    $('.boxclose').click(closeBox);
+  }
+
+  function showModalLogin(){
+    $('#modal').fadeIn();
+  }
+
+  function showModalRegister(){
+    $('#modal').fadeIn();
+    showRegister();
+  }
+
+  function closeBox(){
+    $('#modal').fadeOut();
+  }
+
+  function showRegister(){
+    $('#btn-register').addClass('btn-selected disableClick');
+    $('#btn-login').removeClass('btn-selected disableClick');
+    $('#form-login').hide();
+    $('#form-register').fadeToggle();
+
+  }
+
+  function showLogin(){
+    $('#btn-login').addClass('btn-selected disableClick');
+    $('#btn-register').removeClass('btn-selected disableClick');
+    $('#form-register').hide();
+    $('#form-login').fadeToggle();
+  }
+
+
+})();
+
+function ajax(url, type, data={}, success=r=>console.log(r), dataType='html'){
+  'use strict';
+  $.ajax({url:url, type:type, dataType:dataType, data:data, success:success});
+}

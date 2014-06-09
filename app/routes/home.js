d71bf2eee9857fe65de426c474dd05ae244e2c49
Aaron Johnson <ajohnson13.aj@gmail.com>
2014-06-08 20:11:55 -0500

    tweaked homepage copy and title

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,7 +2,7 @@
 
 exports.index = (req, res)=>{
   if(!req.session.userId){
-    res.render('home/index', {user: res.locals.user, title: 'Node.js: Home'});
+    res.render('home/index', {user: res.locals.user, title: 'SquarePeg: Home'});
   }
   else{
     res.redirect('/users/dash');
